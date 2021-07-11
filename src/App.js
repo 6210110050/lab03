8ea92106d40dbf2c804777d1488f5761570ad076
Jatinut Satsara <you@example.com>
2021-07-11 18:35:27 +0700

    commit 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
 import './App.css';
+import CharacterCard from'./CharacterCard';
 function App() {
  return (
  <div>
- Hello World
+    <CharacterCard value="h"/>
+    <CharacterCard value="i"/>
  </div>
  );
 }
